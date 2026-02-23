b19f0e4db028f9e928d3df5abc9db3fe44d75b18
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-02-23 17:24:55 +0800

    add shadcn

--- client/src/pages/dashboard/page.tsx
+++ client/src/pages/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div>Dashboard</div>
+      <Button>Click me</Button>
+    </>
+  );
 };
