49ffe1d4ca5c35d40607b642f3bb5c5c2063f9c4
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-03-02 21:31:51 +0800

    initial update tracker UI

--- client/src/pages/manage-tracker/id/page.tsx
+++ client/src/pages/manage-tracker/id/page.tsx
@@ -1,9 +1,26 @@
 import { Tracker } from "@/components/tracker/component";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export const EditTracker = () => {
   return (
     <main className="h-screen overflow-auto">
-      <Tracker />
+      <ResizablePanelGroup orientation="horizontal">
+        <ResizablePanel className="p-2" minSize={"20%"} maxSize={"30%"}>
+          Tracker Settings
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          <Tracker />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel className="p-2" minSize={"20%"} maxSize={"30%"}>
+          Component Settings
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   );
 };
